68bc39e6efdedf3070c723ec384ea864330ac884
Niilo Poutanen <niilo.poutanen@gmail.com>
2022-09-13 20:51:13 +0300

    added task cards

--- TODO-app/TODO-app/Resources/Resource.designer.cs
+++ TODO-app/TODO-app/Resources/Resource.designer.cs
@@ -3383,13 +3383,16 @@ namespace TODO_app
 			public const int rounded_corner = 2131165311;
 			
 			// aapt resource value: 0x7F070080
-			public const int tooltip_frame_dark = 2131165312;
+			public const int task_card = 2131165312;
 			
 			// aapt resource value: 0x7F070081
-			public const int tooltip_frame_light = 2131165313;
+			public const int tooltip_frame_dark = 2131165313;
 			
 			// aapt resource value: 0x7F070082
-			public const int top_nav_items = 2131165314;
+			public const int tooltip_frame_light = 2131165314;
+			
+			// aapt resource value: 0x7F070083
+			public const int top_nav_items = 2131165315;
 			
 			static Drawable()
 			{
