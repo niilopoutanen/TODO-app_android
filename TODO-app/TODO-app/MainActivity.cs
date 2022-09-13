66df6c36fe69c465f6c2f7f7414d008ef93fa129
Niilo Poutanen <niilo.poutanen@gmail.com>
2022-09-13 12:49:57 +0300

    updated button

--- TODO-app/TODO-app/MainActivity.cs
+++ TODO-app/TODO-app/MainActivity.cs
@@ -11,17 +11,17 @@ namespace TODO_app
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-        Button btnCreateTask;
-        DatePicker datePicker;
+        //Button btnCreateTask;
+        //DatePicker datePicker;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
-            btnCreateTask = FindViewById<Button>(Resource.Id.CreateTask);
-            datePicker = FindViewById<DatePicker>(Resource.Id.datePicker1);
-            btnCreateTask.Click += btnCreateTask_Click;
+            //btnCreateTask = FindViewById<Button>(Resource.Id.CreateTask);
+            //datePicker = FindViewById<DatePicker>(Resource.Id.datePicker1);
+            //btnCreateTask.Click += btnCreateTask_Click;
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
@@ -32,11 +32,11 @@ namespace TODO_app
 
         private void btnCreateTask_Click(object sender, EventArgs e)
         {
-            Dialog popup = new Dialog(this);
-            popup.SetContentView(Resource.Layout.create_task_popup);
-            popup.Window.SetSoftInputMode(SoftInput.AdjustResize);
-            popup.SetTitle("@string/TaskName");
-            popup.Show();
+            //Dialog popup = new Dialog(this);
+            //popup.SetContentView(Resource.Layout.create_task_popup);
+            //popup.Window.SetSoftInputMode(SoftInput.AdjustResize);
+            //popup.SetTitle("@string/TaskName");
+            //popup.Show();
         }
     }
 }
--- TODO-app/TODO-app/Resources/Resource.designer.cs
+++ TODO-app/TODO-app/Resources/Resource.designer.cs
@@ -4553,49 +4553,52 @@ namespace TODO_app
 			public const int date_select_header = 2131558435;
 			
 			// aapt resource value: 0x7F0D0024
-			public const int fab_transformation_scrim_behavior = 2131558436;
+			public const int default_description = 2131558436;
 			
 			// aapt resource value: 0x7F0D0025
-			public const int fab_transformation_sheet_behavior = 2131558437;
+			public const int fab_transformation_scrim_behavior = 2131558437;
 			
 			// aapt resource value: 0x7F0D0026
-			public const int fallback_menu_item_copy_link = 2131558438;
+			public const int fab_transformation_sheet_behavior = 2131558438;
 			
 			// aapt resource value: 0x7F0D0027
-			public const int fallback_menu_item_open_in_browser = 2131558439;
+			public const int fallback_menu_item_copy_link = 2131558439;
 			
 			// aapt resource value: 0x7F0D0028
-			public const int fallback_menu_item_share_link = 2131558440;
+			public const int fallback_menu_item_open_in_browser = 2131558440;
 			
 			// aapt resource value: 0x7F0D0029
-			public const int hide_bottom_view_on_scroll_behavior = 2131558441;
+			public const int fallback_menu_item_share_link = 2131558441;
 			
 			// aapt resource value: 0x7F0D002A
-			public const int mtrl_chip_close_icon_content_description = 2131558442;
+			public const int hide_bottom_view_on_scroll_behavior = 2131558442;
 			
 			// aapt resource value: 0x7F0D002B
-			public const int password_toggle_content_description = 2131558443;
+			public const int mtrl_chip_close_icon_content_description = 2131558443;
 			
 			// aapt resource value: 0x7F0D002C
-			public const int path_password_eye = 2131558444;
+			public const int password_toggle_content_description = 2131558444;
 			
 			// aapt resource value: 0x7F0D002D
-			public const int path_password_eye_mask_strike_through = 2131558445;
+			public const int path_password_eye = 2131558445;
 			
 			// aapt resource value: 0x7F0D002E
-			public const int path_password_eye_mask_visible = 2131558446;
+			public const int path_password_eye_mask_strike_through = 2131558446;
 			
 			// aapt resource value: 0x7F0D002F
-			public const int path_password_strike_through = 2131558447;
+			public const int path_password_eye_mask_visible = 2131558447;
 			
 			// aapt resource value: 0x7F0D0030
-			public const int search_menu_title = 2131558448;
+			public const int path_password_strike_through = 2131558448;
 			
 			// aapt resource value: 0x7F0D0031
-			public const int status_bar_notification_info_overflow = 2131558449;
+			public const int search_menu_title = 2131558449;
 			
 			// aapt resource value: 0x7F0D0032
-			public const int task_name_header = 2131558450;
+			public const int status_bar_notification_info_overflow = 2131558450;
+			
+			// aapt resource value: 0x7F0D0033
+			public const int task_name_header = 2131558451;
 			
 			static String()
 			{
