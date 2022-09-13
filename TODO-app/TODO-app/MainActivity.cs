ba87023dbdc8dddae6df15841ba95e40c4c34777
osaama05 <oskari.makela@hotmail.com>
2022-09-13 10:28:37 +0300

    Button create

--- TODO-app/TODO-app/MainActivity.cs
+++ TODO-app/TODO-app/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Runtime;
+using Android.Widget;
 using AndroidX.AppCompat.App;
 
 namespace TODO_app
@@ -8,12 +9,14 @@ namespace TODO_app
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        Button btnCreateTask;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            btnCreateTask = FindViewById<Button>(Resource.Id.CreateTask);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
@@ -21,5 +24,7 @@ namespace TODO_app
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
+
+
     }
 }
