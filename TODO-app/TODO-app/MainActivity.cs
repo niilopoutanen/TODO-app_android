db6bd92eef0571aac9e995f5819040f9eec0a077
osaama05 <oskari.makela@hotmail.com>
2022-09-14 11:00:50 +0300

    Some functionality

--- TODO-app/TODO-app/MainActivity.cs
+++ TODO-app/TODO-app/MainActivity.cs
@@ -16,7 +16,7 @@ namespace TODO_app
         private Button btnAddTask;
         private DatePicker datePicker;
         private EditText TaskNameInput;
-        private List<TaskItem> taskList;
+        private List<TaskItem> taskList = new List<TaskItem>();
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -24,11 +24,10 @@ namespace TODO_app
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
 
-            TaskNameInput = FindViewById<EditText>(Resource.Id.NameInputForm);
+
             btnCreateTask = FindViewById<Button>(Resource.Id.CreateTask);
             btnCreateTask.Click += btnCreateTask_Click;
-            btnAddTask = FindViewById<Button>(Resource.Id.AddButton);
-            btnAddTask.Click += btnTaskNameSubmit_Click;
+
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
@@ -39,18 +38,16 @@ namespace TODO_app
 
         private void btnCreateTask_Click(object sender, EventArgs e)
         {
-            Dialog popup = new Dialog(this);
-            popup.SetContentView(Resource.Layout.create_task_popup);
-            popup.Window.SetSoftInputMode(SoftInput.AdjustResize);
-            popup.SetTitle("New Task");
-            popup.Show();
+            SetContentView(Resource.Layout.create_task_popup);
+            TaskNameInput = FindViewById<EditText>(Resource.Id.NameInputForm);
+            btnAddTask = FindViewById<Button>(Resource.Id.AddButton);
+            btnAddTask.Click += btnAddTas_Click;
         }
 
-        private void btnTaskNameSubmit_Click(object sender, EventArgs e)
+        private void btnAddTas_Click(object sender, EventArgs e)
         {
             TaskItem task = new TaskItem();
             task.Text = TaskNameInput.Text;
-            task.DueDate = datePicker.DateTime;
             taskList.Add(task);
         }
 
