dea50fcdad1d6e790f8184b7ff80e8b70b9f69a3
Tolpanjuuri <tomi.vahdersalo@gmail.com>
2022-09-15 08:55:12 +0300

    settings class

---
+++ TODO-app/TODO-app/Settings.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TODO_app
+{
+    internal class Settings
+    {
+        private bool Darkmode = true;
+    }
+}
