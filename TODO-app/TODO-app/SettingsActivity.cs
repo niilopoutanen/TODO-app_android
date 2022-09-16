e3ae0ba5c491c094b765b32cf8b50137e9fb9e4a
Niilo Poutanen <niilo.poutanen@gmail.com>
2022-09-16 18:08:02 +0300

    Revert "widget testing, doesn't work yet"
    This reverts commit df7f2984672d4acb2bcd44da9a74c4c2f91c7b56.

--- TODO-app/TODO-app/SettingsActivity.cs
+++ TODO-app/TODO-app/SettingsActivity.cs
@@ -2,9 +2,14 @@
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Views;
 using Android.Widget;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using AndroidX.AppCompat.App;
+using System.Runtime.Remoting.Contexts;
 using Xamarin.Essentials;
 
 namespace TODO_app
