de75c2c3890fda8fa1e02b7389396a73f5a8a090
osaama05 <oskari.makela@hotmail.com>
2022-09-27 09:27:04 +0300

    Deleted useless things

--- TODO-app/TODO-app/MainActivity.cs
+++ TODO-app/TODO-app/MainActivity.cs
@@ -87,12 +87,6 @@ namespace TODO_app
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            //GoogleAnalytics.Current.Config.TrackingId = "4095321217";
-            //GoogleAnalytics.Current.Config.AppId = "com.tiimi1.todo_app";
-            //GoogleAnalytics.Current.Config.AppName = "TODO-app";
-            //GoogleAnalytics.Current.Config.AppVersion = "1.3.0.0";
-            //GoogleAnalytics.Current.InitTracker();
-            //GoogleAnalytics.Current.Tracker.SendView("MainPage");
             FirebaseAnalytics.GetInstance(this);
             LoadSettings();
             base.OnCreate(savedInstanceState);
