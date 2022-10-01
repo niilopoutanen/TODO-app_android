29b81cf6c1fa576ab03e667c87930981111f4893
Niilo Poutanen <niilo.poutanen@gmail.com>
2022-10-01 20:10:01 +0300

    Update ActivityMethods.cs

--- TODO-app/TODO-app/ActivityMethods.cs
+++ TODO-app/TODO-app/ActivityMethods.cs
@@ -3,7 +3,9 @@ using Android.Content;
 using Android.Graphics.Drawables;
 using Android.OS;
 using Android.Runtime;
+using Android.Util;
 using Android.Views;
+using AndroidX.AppCompat.App;
 using Android.Widget;
 using System;
 using System.Collections.Generic;
@@ -12,9 +14,14 @@ using System.Text;
 
 namespace TODO_app
 {
-    internal class ActivityMethods
+    internal class ActivityMethods : AppCompatActivity
     {
+        public int DpToPx(int dpValue)
+        {
 
-
+            //viittaaminen toiseen luokkaan ei onnistu, Java.NullException
+            int pixel = (int)TypedValue.ApplyDimension(ComplexUnitType.Dip, dpValue, Resources.DisplayMetrics);
+            return pixel;
+        }
     }
 }
