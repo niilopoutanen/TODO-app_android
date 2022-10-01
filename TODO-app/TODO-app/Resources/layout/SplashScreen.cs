b527c57e5f2ec68c0d83e3614a797d19b6122ed1
Niilo Poutanen <niilo.poutanen@gmail.com>
2022-10-01 19:22:51 +0300

    working splash screen

--- TODO-app/TODO-app/Resources/layout/SplashScreen.cs
+++ TODO-app/TODO-app/Resources/layout/SplashScreen.cs
@@ -17,25 +17,15 @@ namespace TODO_app.Resources.layout
     [Activity(Theme = "@style/AppTheme.Splash", MainLauncher = true, NoHistory = true)]
     public class SplashActivity : AppCompatActivity
     {
-        static readonly string TAG = "X:" + typeof(SplashActivity).Name;
 
         public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
         {
             base.OnCreate(savedInstanceState, persistentState);
-            Log.Debug(TAG, "SplashActivity.OnCreate");
         }
 
-        // Launches the startup task
         protected override void OnResume()
         {
             base.OnResume();
-            Task startupWork = new Task(() => { SimulateStartup(); });
-            startupWork.Start();
-        }
-
-        // Simulates background work that happens behind the splash screen
-        void SimulateStartup()
-        {
             StartActivity(new Intent(Application.Context, typeof(MainActivity)));
         }
 
