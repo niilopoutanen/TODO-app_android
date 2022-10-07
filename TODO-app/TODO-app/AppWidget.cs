7964ad84bc6995bf3baab17361b10f2caef7d7a9
Niilo Poutanen <niilo.poutanen@gmail.com>
2022-10-07 20:29:18 +0300

    working widget initialization

---
+++ TODO-app/TODO-app/AppWidget.cs
@@ -0,0 +1,51 @@
+﻿using Android.App;
+using Android.Appwidget;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TODO_app
+{
+    [BroadcastReceiver(Label = "TODO-app widget", Exported =true)]
+    [IntentFilter(new string[] { "android.appwidget.action.APPWIDGET_UPDATE" })]
+    [MetaData("android.appwidget.provider", Resource = "@xml/appwidgetprovider")]
+
+    internal class AppWidget : AppWidgetProvider
+    {
+        public override void OnUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds)
+        {
+            var me = new ComponentName(context, Java.Lang.Class.FromType(typeof(AppWidget)).Name);
+            appWidgetManager.UpdateAppWidget(me, BuildRemoteViews(context, appWidgetIds));
+        }
+
+        public override void OnReceive(Context context, Intent intent) { }
+
+        private RemoteViews BuildRemoteViews(Context context, int[] appWidgetIds)
+        {
+            var widgetView = new RemoteViews(context.PackageName, Resource.Layout.widget);
+            SetTextViewText(widgetView);
+            RegisterClicks(context, appWidgetIds, widgetView);
+
+            return widgetView;
+        }
+        private void SetTextViewText(RemoteViews widgetView)
+        {
+            widgetView.SetTextViewText(Resource.Id.widgetHeader, "HelloAppWidget");
+        }
+        private void RegisterClicks(Context context, int[] appWidgetIds, RemoteViews widgetView)
+        {
+            var intent = new Intent(context, typeof(AppWidget));
+            intent.SetAction(AppWidgetManager.ActionAppwidgetUpdate);
+            intent.PutExtra(AppWidgetManager.ExtraAppwidgetIds, appWidgetIds);
+
+            var piBackground = PendingIntent.GetBroadcast(context, 0, intent, PendingIntentFlags.UpdateCurrent);
+            widgetView.SetOnClickPendingIntent(Resource.Id.widgetBG, piBackground);
+        }
+    }
+}
