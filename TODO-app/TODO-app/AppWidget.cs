2f932401d63b99b974ba0ee876ea2731956a1c9a
Niilo Poutanen <niilo.poutanen@gmail.com>
2022-10-08 13:23:28 +0300

    throws some error, should work

--- TODO-app/TODO-app/AppWidget.cs
+++ TODO-app/TODO-app/AppWidget.cs
@@ -3,6 +3,7 @@ using Android.Appwidget;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Util;
 using Android.Views;
 using Android.Widget;
 using System;
@@ -12,7 +13,7 @@ using System.Text;
 
 namespace TODO_app
 {
-    [BroadcastReceiver(Label = "TODO-app widget", Exported =true)]
+    [BroadcastReceiver(Label = "TODO-app widget", Exported = true)]
     [IntentFilter(new string[] { "android.appwidget.action.APPWIDGET_UPDATE" })]
     [MetaData("android.appwidget.provider", Resource = "@xml/appwidgetprovider")]
 
@@ -23,20 +24,33 @@ namespace TODO_app
             var me = new ComponentName(context, Java.Lang.Class.FromType(typeof(AppWidget)).Name);
             appWidgetManager.UpdateAppWidget(me, BuildRemoteViews(context, appWidgetIds));
         }
-
-        public override void OnReceive(Context context, Intent intent) { }
-
         private RemoteViews BuildRemoteViews(Context context, int[] appWidgetIds)
         {
             var widgetView = new RemoteViews(context.PackageName, Resource.Layout.widget);
+
             SetTextViewText(widgetView);
             RegisterClicks(context, appWidgetIds, widgetView);
-
             return widgetView;
         }
         private void SetTextViewText(RemoteViews widgetView)
         {
-            widgetView.SetTextViewText(Resource.Id.widgetHeader, "HelloAppWidget");
+            FileClass files = new FileClass();
+            int taskNotDoneCount = 0;
+            List<TaskItem> taskList = new List<TaskItem>();
+            taskList = files.ReadFile();
+            for (int i = 0; i < taskList.Count; i++)
+            {
+                if (taskList[i].DueDate == DateTime.Today)
+                {
+                    if (taskList[i].IsDone == false)
+                    {
+                        taskNotDoneCount++;
+                    }
+
+                }
+
+            }
+            widgetView.SetTextViewText(Resource.Id.widgetCount, taskNotDoneCount.ToString());
         }
         private void RegisterClicks(Context context, int[] appWidgetIds, RemoteViews widgetView)
         {
@@ -44,6 +58,7 @@ namespace TODO_app
             intent.SetAction(AppWidgetManager.ActionAppwidgetUpdate);
             intent.PutExtra(AppWidgetManager.ExtraAppwidgetIds, appWidgetIds);
 
+            // Register click event for the Background
             var piBackground = PendingIntent.GetBroadcast(context, 0, intent, PendingIntentFlags.UpdateCurrent);
             widgetView.SetOnClickPendingIntent(Resource.Id.widgetBG, piBackground);
         }
