263dab44472880f5790e83c6ea9a9d13e03b7333
Niilo Poutanen <niilo.poutanen@gmail.com>
2022-10-16 11:21:26 +0300

    added changelog layout

---
+++ TODO-app/TODO-app/WhatsNewActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TODO_app
+{
+    [Activity(Label = "WhatsNewActivity", Theme = "@style/AppTheme")]
+    public class WhatsNewActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
